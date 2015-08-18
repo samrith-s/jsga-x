b30ce824d67c39c5dc82be15281fcb9bab9668ca
Samrith Shankar <samrith@ptotem.com>
2015-08-18 16:47:59 +0530

    Commenting started

--- app/currency.js
+++ app/currency.js
@@ -1,3 +1,9 @@
+/**
+ * @typedef optionsHash
+ * @type {object}
+ * @property {function} callback A custom callback function callable at any point
+ * @property {string} icon The icon for the currency - generally a string path of image
+ */
 
 var Currencies={};
 
@@ -9,10 +15,17 @@ Currencies.find = function(name) {
 			return Currencies.all[i];
 }
 
-
-
-/*------- CURRENCY -------*/
-
+/**
+ * @class
+ * 
+ *	Currency class that allows creation of new currencies like health, coins etc and allows effective management. Part of the game logic element.
+ * 
+ * @param {string} name - A unique name for the currency
+ * @param {int} min - Minimum value of currency
+ * @param {int} max - Maximum value of currency
+ * @param {int} value - Value to initialize currency with
+ * @param {optionsHash} options
+ */
 function Currency(name, min, max, value, options) {
 
 	var settings = $.extend({
@@ -38,12 +51,32 @@ function Currency(name, min, max, value, options) {
 	return this;
 }
 
+/**
+	increase(value) -  Function to increase the currency by a certain value
+
+	Parameters:
+	value - Value to increase the currency by
+
+	Returns:
+	The current object after increasing value
+*/
+
 Currency.prototype.increase = function(value) {
 	this.value += value;
 	this.callback(value);
 	
 	return this;
 }
+
+/**
+	decrease(value) - Function to decrease the currency by a certain value
+
+	Parameters:
+	value - Value to decrease the currency by
+
+	Returns:
+	The currenct object after decreasing value
+*/
 
 Currency.prototype.decrease = function(value) {
 	this.value -= value;
@@ -52,12 +85,26 @@ Currency.prototype.decrease = function(value) {
 	return this;
 }
 
+/**
+	isMax() - Function to check if currency is at it's maximum
+
+	Returns:
+	True or false
+*/
+
 Currency.prototype.isMax = function() {
 	if(this.value===this.max)
 		return true;
 	else
 		return false;
 }
+
+/**
+	isMin() - Function to check if currency is at it's minimum
+
+	Returns:
+	True or false
+*/
 
 Currency.prototype.isMin = function() {
 	if(this.value===this.min)
